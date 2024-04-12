b2b893836a7d5d19b1e3a1257bb6f717563fd16b
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-04-12 15:46:00 +0200

    [add] Filtered Paragraph

--- index.php
+++ index.php
@@ -4,8 +4,27 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Badwords</title>
+
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
-<body>
-  <h1>Ciao PHP</h1>
+<body class="bg-danger-subtle">
+    
+  <form action="result_page.php" method="POST" class="container pt-5">
+    
+    <div class="py-5">
+      <label class="form-label ms-3 fw-bold">Scrivi una frase</label>
+      <textarea name="paragraph" class="form-control bg-info-subtle" placeholder="Leave a comment here"></textarea>
+    </div>
+
+    <div class="mb-3">
+      <label class="form-label ms-3 fw-bold">Inserisci una parola da censurare</label>
+      <input name="banned_word" type="textarea" class="form-control bg-warning-subtle">
+    </div>
+
+    <button type="submit" class="btn btn-light btn-outline-danger">Submit</button>
+  </form>
+
 </body>
 </html>
---
+++ result_page.php
@@ -0,0 +1,46 @@
+<?php
+
+  $paragraph = $_POST['paragraph'];
+  $banned_word = $_POST['banned_word'];
+  
+  $length_paragraph = strlen($paragraph);
+  $censored_paragraph = str_replace($banned_word,'***', $paragraph);
+  $length_censored_paragraph = strlen($censored_paragraph);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.css' integrity='sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==' crossorigin='anonymous'/>
+
+  <title>Ricevo from GET</title>
+</head>
+<body class="bg-primary-subtle">
+
+  <div class="container py-5 text-center">
+
+    <div class="bg-info-subtle p-4">
+      <h2>Paragrafo Originale</h2>
+      <p class="fs-4 py-3"><?php echo $paragraph ?></p>
+      <p><strong>Lunghezza:</strong> <?php echo $length_paragraph ?> caratteri</p>
+    </div>
+
+    <div class="bg-danger-subtle p-4">
+      <h2>Parola da Censurare</h2>
+      <p class="fs-4 py-3"><?php echo $banned_word ?></p>
+    </div>
+
+    <div class="bg-warning-subtle p-4">
+      <h2>Paragrafo Censurato</h2>
+      <p class="fs-4 py-3"><?php echo $censored_paragraph ?></p>
+      <p><strong>Lunghezza:</strong> <?php echo $length_censored_paragraph ?> caratteri</p>
+    </div>
+
+  </div>
+  
+</body>
+</html>
